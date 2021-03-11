60cb8e63277d20d939e43045e24e3894593fbd71
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-03-10 19:28:17 -0600

    set up function to grab id

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+var tmdbKey = 'cae9a497d1eb142f3801452347747341';
+
+var nyKey = 'TTYU09cHbS77MqlmFfWwC8unridSAyP2';
+
+$(document).ready(function(){
+    $(document).click(function(event){
+        event.preventDefault();
+        console.log(event.target.id)
+        });
+    // $('#animated').click(function(e){
+    //     e.preventDefault();
+
+        
+
+    //     $.ajax({
+    //         url:             
+    //     }).then(function(response){
+    //         console.log(response);
+        
+    //     }) 
+    // });
+});
