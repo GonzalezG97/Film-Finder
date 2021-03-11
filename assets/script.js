b7c07af3e1467bcb38899a9f187253a4cf8547af
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-03-10 20:36:03 -0600

    set up function to use userchoice for api pull

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,14 @@ var tmdbKey = 'cae9a497d1eb142f3801452347747341';
 
 var nyKey = 'TTYU09cHbS77MqlmFfWwC8unridSAyP2';
 
-$(document).ready(function(){
-    $(document).click(function(event){
-        event.preventDefault();
-        console.log(event.target.id)
-        });
-    // $('#animated').click(function(e){
-    //     e.preventDefault();
 
-        
+$(document).ready(function(){$(document).click(function(event){
+    
+    var userChoice = event.target.id
+    $.ajax({
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=cae9a497d1eb142f3801452347747341&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${userChoice}`          
+    }).then(function(response){
+        console.log(response);
+    })
 
-    //     $.ajax({
-    //         url:             
-    //     }).then(function(response){
-    //         console.log(response);
-        
-    //     }) 
-    // });
-});
+})});
