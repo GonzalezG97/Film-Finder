05eaf2cf734a7d5a5d2febdd07e7be0aff83f22a
Bridget S <stocktonbridget@gmail.com>
2021-03-13 12:58:35 -0600

    wrong changes

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,21 @@ var nyKey = 'TTYU09cHbS77MqlmFfWwC8unridSAyP2';
 
 
 $(document).ready(function(){$(document).click(function(event){
-    event.preventDefault();
+    // event.preventDefault();
     var userChoice = event.target.id
+    console.log(userChoice);
     $.ajax({
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${userChoice}`          
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${userChoice}`
     }).then(function(response){
         var movies = response.results;
         console.log(movies);
         $.each(movies, function(index, movie){
-            
+
             const { title, overview} = movie;
-            
-            $('#watchList').append('<div>' + title + '<br>' + overview + '</div>' + '<br>');
+
+            $('#displayDiv').append('<div>' + title + '<br>' + overview + '</div>' + '<br>');
         });
     });
-    
+
 
 })});
