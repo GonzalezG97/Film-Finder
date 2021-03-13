28ab5d3fb1e422caa3484c7564aaddf5e22701ed
Bridget S <stocktonbridget@gmail.com>
2021-03-13 13:34:08 -0600

    idk

--- assets/script.js
+++ assets/script.js
@@ -3,31 +3,16 @@ var tmdbKey = 'cae9a497d1eb142f3801452347747341';
 var nyKey = 'TTYU09cHbS77MqlmFfWwC8unridSAyP2';
 
 
-<<<<<<< HEAD
-$(document).ready(function(){$(document).click(function(event){
-    // event.preventDefault();
-    var userChoice = event.target.id
-=======
 $(document).ready(function(){
     $('.genreMovie').click(
         function(event){
     
     var userChoice = event.target.id
     console.log(event);
->>>>>>> a25804362d915c4a7e3c20f912479f22218ec40e
     console.log(userChoice);
     $.ajax({
         url: `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${userChoice}`
     }).then(function(response){
-<<<<<<< HEAD
-        var movies = response.results;
-        console.log(movies);
-        $.each(movies, function(index, movie){
-
-            const { title, overview} = movie;
-
-            $('#displayDiv').append('<div>' + title + '<br>' + overview + '</div>' + '<br>');
-=======
         var genre = response.results;
         console.log(genre);
 
@@ -55,17 +40,12 @@ $(document).ready(function(){
             console.log($(this).attr('data-title'));
            $(`<p>${JSON.stringify(data)}</p>`).appendTo('#savedMovies');
             
->>>>>>> a25804362d915c4a7e3c20f912479f22218ec40e
         });
 
         
     });
-<<<<<<< HEAD
-
-=======
     
     
 
 })});
->>>>>>> a25804362d915c4a7e3c20f912479f22218ec40e
 
