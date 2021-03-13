e0054f57757f704ea98344006304bb0cd3d842dc
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-03-13 12:14:20 -0600

    Function to load results and save to watchlist

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,48 @@ var tmdbKey = 'cae9a497d1eb142f3801452347747341';
 var nyKey = 'TTYU09cHbS77MqlmFfWwC8unridSAyP2';
 
 
-$(document).ready(function(){$(document).click(function(event){
+$(document).ready(function(){
+    $('.genreMovie').click(
+        function(event){
     
     var userChoice = event.target.id
+    console.log(event);
     console.log(userChoice);
     $.ajax({
         url: `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${userChoice}`          
     }).then(function(response){
         var genre = response.results;
         console.log(genre);
-        $.each(genre, function(index, movies){
-            
-            const { title, overview} = movies;
-            
-            $('#displayDiv').append('<section>' + title + '</section>' + '<div>' + overview + '<br>' + '<button>' + 'Save' + '</button>' +'</div>' + '<br>');
 
+        $('#displayDiv').empty();
+
+        for (var i = 0; i < genre.length; i++ ){
+            const {title, overview} = genre[i];           
+            console.log(genre[i]);
+           let moviesSelection = $(`<div>`);
+           
+           moviesSelection.addClass('movieListItem');           
+
+           moviesSelection.append(`<h1>${title}</h1>`);
+
+           moviesSelection.append(`<p>${overview}</p>`);
+
+           moviesSelection.append(`<button class='saveBtn' data-title='${genre[i].title}'>Save to Watchlist</button>`);
+
+           moviesSelection.appendTo('#displayDiv');
+        };
+        
+        
+        $('.saveBtn').click(function(){
+            let data = $(this).data();
+            console.log($(this).attr('data-title'));
+           $(`<p>${JSON.stringify(data)}</p>`).appendTo('#savedMovies');
             
         });
-        
+
         
     });
+    
     
 
 })});
